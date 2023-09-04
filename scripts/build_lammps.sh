155e3ca60b5b1e5db59cc5fe86966752b7b49a2c
kermodegroup <james.kermode@gmail.com>
2023-09-04 13:17:08 +0000

    Update from https://github.com/kermodegroup/wiki/commit/ff33c8ad94f0ea60d4fc21604384d929760a6a12

---
+++ scripts/build_lammps.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+cd mylammps
+​
+mkdir build
+​
+cd build
+
+
+cmake ../cmake -D CMAKE_BUILD_TYPE=Release -D BUILD_SHARED_LIBS=yes -D CMAKE_INSTALL_PREFIX=$VIRTUAL_ENV \
+               -D PKG_PYTHON=yes -D PKG_ML-PACE=yes -D PKG_OPENMP=yes \
+               -D BUILD_MPI=yes \
+
+​
+cmake --build . -j 40
+​
+cmake --install .
+​
+cd ..
+​
+rm -r -d build
+​
+cd ..
+​
+
+echo 'export LD_LIBRARY_PATH=$VIRTUAL_ENV/lib64:$LD_LIBRARY_PATH' >> $VIRTUAL_ENV/bin/activate
+echo 'export LAMMPS_POTENTIALS=$VIRTUAL_ENV/share/lammps/potentials/' >> $VIRTUAL_ENV/bin/activate
