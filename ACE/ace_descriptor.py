ea4b5181840bafd2d394fd7400e2d2a0742efa53
kermodegroup <james.kermode@gmail.com>
2025-04-22 14:57:35 +0000

    Update from https://github.com/kermodegroup/wiki/commit/ebbdb503ca80e3ef04da3214736bb18d1130a0e8

--- ACE/ace_descriptor.py
+++ ACE/ace_descriptor.py
@@ -1,78 +1,46 @@
-import numpy as np
-from julia.api import Julia
-from ase.atoms import Atoms
-jl = Julia(compiled_modules=False)
+def ace_descriptor(species, **kwargs):
+    '''
+    Generate a simple callable interface for an ACE descriptor using Julia and ACE1pack.jl
 
-from julia import Main
-Main.eval('include("ace_descriptor.jl")')
-Main.eval("using ASE, JuLIP, ACE1pack, .ace_descriptor")
-convert = Main.eval("julip_at(a) = JuLIP.Atoms(a)")
-ASEAtoms = Main.eval("ASEAtoms(a) = ASE.ASEAtoms(a)")
+    species: list of str
+        List of chemical symbols (e.g. ["Si"])
 
-jl_ace = Main.eval("get_ace(species, N, maxdeg, rcut) = ace_descriptor.ACE(species, N, maxdeg, rcut)")
-jl_get_vecs = Main.eval("get_vecs(ace, ats) = ace_descriptor.get_ace_vecs(ace, ats)")
-jl_get_basis_len = Main.eval("get_len(ace) = length(ace)")
+    kwargs: 
+        Extra keyword args passed directly to ACE1x.ace_basis
+        See https://acesuit.github.io/ACEpotentials.jl/dev/literate_tutorials/TiAl_basis/ for list of args
+    '''
+    from julia import Main
+    Main.eval("using ASE, JuLIP, ACEpotentials")
 
-def _ACE(species, N=3, max_deg=3, rcut=5.0):
-    ace = jl_ace(species, N, max_deg, rcut)
-    return ace
+    ace_func = Main.eval(f"basis(species; kwargs...) = ACE1x.ace_basis(elements=Symbol.(species); kwargs...)")
+    ace = ace_func(species, **kwargs)
 
-def get_ace_vecs(ace, atoms):
-    jl_ats = ASEAtoms(atoms)
-    jl_ats = convert(jl_ats)
+    
+    ASE_atoms = Main.eval("ASEAtoms(a) = ASE.ASEAtoms(a)")
+    ase_to_julip_atoms = Main.eval("julip_at(a) = JuLIP.Atoms(a)")
 
-    vecs = jl_get_vecs(ace, jl_ats)
+    py_to_julip_atoms = lambda struct: ase_to_julip_atoms(ASE_atoms(struct))
 
-    return vecs
+    eval_ace = Main.eval('''
+        function get_ace_vecs(ace, atoms)
+            descriptor = zeros((length(atoms), length(ace)))
 
+            for i in 1:length(atoms)
+                descriptor[i, :] = site_energy(ace, atoms, i)
+            end
+            return descriptor
+        end
+    
+    ''')
 
-
-class ACE():
-    def __init__(self, species, N=3, max_deg=3, rcut=5.0):
+    def calc_descriptor(atoms):
         '''
-        Build a Julia ACE model with a Python interface
-        Intended only for computing ACE Descriptor vectors on a structure
-
-        ### Args ###
-        species : List of strings; Chemical symbols for the ACE model
-                    EG: species=["In", "P"]
-
-        N       : int; Correlation order
-
-        max_deg : int; Maximum polynomial degree
-
-        rcut    : float; Radial cutoff in Ang for the basis
+        Inner func to get descriptor vectors given an atoms object
         '''
-        self.species = species
-        self.N = N
-        self.max_deg = max_deg
-        self.rcut = rcut
-        self.desc = _ACE(species, N, max_deg, rcut)
-        self.basis_len = jl_get_basis_len(self.desc)
+        jl_ats = py_to_julip_atoms(atoms)
 
-    def get_ace_vecs_single(self, struct):
-        return get_ace_vecs(self.desc, struct)
+        descriptors = eval_ace(ace, jl_ats)
 
-    def get_ace_vecs_dataset(self, images):
-        return [self.get_ace_vecs_single(image) for image in images]
+        return descriptors
 
-    def __call__(self, atoms):
-        if type(atoms) == Atoms:
-            # Single image
-            return self.get_ace_vecs_single(atoms)
-        elif type(atoms) == list:
-            # List of images
-            return self.get_ace_vecs_dataset(atoms)
-
-    def __len__(self):
-        return self.basis_len
-
-
-
-if __name__ == "__main__":
-    from ase.io import read
-    bulk = read("Bulk/ZB_Bulk.xyz", index="-1")
-
-    ace = ACE(["In", "P"], N=3, max_deg=3, rcut=5.0)
-
-    print(ace(bulk).shape)
+    return calc_descriptor
